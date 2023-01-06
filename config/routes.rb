44c530896b8c6b980f25dddba064f614e2a58db6
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-01-06 19:40:44 +0900

    Add:login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
-  get 'users/new'
+  root to: 'posts#index'
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+  resources :users, only: %i[new create]
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
