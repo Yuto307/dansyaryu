703fe8513a684c9efcc0fd7ab0f41e3698e5abac
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-01-12 23:50:01 +0900

    second:commit

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -36,6 +36,17 @@ class PostsController < ApplicationController
     @comments = @post.comments.includes(:user).order(created_at: :desc)
   end
 
+  def edit; end
+
+  def update
+    if @post.update(post_params)
+      redirect_to @post, success: t('defaults.message.updated', item: Post.model_name.human)
+    else
+      flash.now[:danger] = t('defaults.message.not_updated', item: Post.model_name.human)
+      render :edit
+    end
+  end
+
   def destroy
     @post.destroy!
     redirect_to posts_path, success: t('defaults.message.deleted', item: Post.model_name.human)
@@ -57,7 +68,7 @@ class PostsController < ApplicationController
     @post = Post.find_by(id: params[:id])
     return unless @post.user_id != @current_user.id
 
-    redirect_to boards_path
+    redirect_to posts_path
   end
 
 end
--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,38 @@
 class VotesController < ApplicationController
+  before_action :set_vote, only: %i[edit destroy]
+  def create
+    @vote = current_user.votes.create(vote_params)
+  end
+
+  def edit; end
+
+  def update
+    @vote = current_user.votes.find(params[:id])
+    if @vote.update(vote_update_params)
+      render json: { vote: @vote }, status: :ok
+    else
+      render json: { vote: @vote, errors:
+      { messages: @vote.errors.full_messages } }, 
+      status: :bad_request
+    end
+  end
+
+  def destroy
+    @vote = current_user.votes.find(params[:id]) # current_userのコメント
+    @vote.destroy!
+  end
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
-  def set_board
-    @board = current_user.boards.find(params[:id])
+  def vote_params
+    params.require(:vote).permit(:status).merge(post_id: params[:post_id])
   end
 
-  # Only allow a trusted parameter "white list" through.
-  def vote_params
-    params.require(:vote).permit(:status).merge(user_id: current_user.id)
+  def set_vote
+    @vote = Vote.find_by(id: params[:id])
+  end
+
+  def vote_update_params
+    params.require(:vote).permit(:content)
   end
 end
