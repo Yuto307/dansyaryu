e78461166e26a93a7bb06450a57f51904127ebc0
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-01-18 23:38:52 +0900

    add_category

--- config/routes.rb
+++ config/routes.rb
@@ -14,4 +14,12 @@ Rails.application.routes.draw do
     resources :favorites, only: %i[create destroy]
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  namespace :admin do
+    root to: 'dashboards#index'
+    get 'login', to: 'user_sessions#new'
+    post 'login', to: 'user_sessions#create'
+    delete 'logout', to: 'user_sessions#destroy'
+    resources :users, only: %i[index edit show update destroy]
+    resources :boards, only: %i[index edit show update destroy]
+  end
 end
