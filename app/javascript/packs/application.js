6ae7fa50eb01f99ca9f0f78ed76b1cecf399c828
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-01-27 23:17:18 +0900

    add:comment

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,14 +3,14 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import '@fortawesome/fontawesome-free/js/all';
-
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import 'bootstrap'
 import '../src/application.scss'
+
+import '@fortawesome/fontawesome-free/js/all';
 
 require("@rails/ujs").start()
 Rails.start()
