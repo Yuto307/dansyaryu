d9575372707703eb78e48d3e3343fe04497bab67
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-01-31 21:40:36 +0900

    Fix:ajax

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,34 +1,31 @@
 class CommentsController < ApplicationController
   before_action :set_comment, only: %i[edit destroy]
   def create
-    @comment = current_user.comments.build(comment_params)
-    @comment.save
-    redirect_back fallback_location: root_path
+    @comment = current_user.comments.create(comment_params)
   end
 
   def edit; end
 
   def update
     @comment = current_user.comments.find(params[:id])
-    if @comment.update(comment_update_params)
-      render json: { comment: @comment }, status: :ok
-    else
-      render json: { comment: @comment, errors:
-      { messages: @comment.errors.full_messages } }, 
-      status: :bad_request
+      if @comment.update(comment_update_params)
+        render json: { comment: @comment }, status: :ok
+      else
+        render json: { comment: @comment, errors:
+          { messages: @comment.errors.full_messages } }, 
+          status: :bad_request
+      end
     end
-  end
 
   def destroy
     @comment = current_user.comments.find(params[:id]) # current_userのコメント
     @comment.destroy!
-    redirect_back fallback_location: root_path
   end
 
   private
 
   def comment_params
-    params.require(:comment).permit(:content, :status).merge(post_id: params[:post_id])
+    params.require(:comment).permit(:content).merge(post_id: params[:post_id])
   end
 
   def set_comment
