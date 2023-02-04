2d431d5f3b5a34c1a92ef28047fb520de9f07a6b
Yuto307 <104293938+Yuto307@users.noreply.github.com>
2023-02-04 16:37:40 +0900

    Revert "giveup:vote"

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,7 +1,24 @@
 class VotesController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @vote = current_user.votes.create(vote_params)
+    if status == 'trash'
+      current_user.trash(@post)
+      redirect_back fallback_location: root_path
+    elsif status == 'untrash'
+      current_user.untrash(@post)
+      redirect_back fallback_location: root_path
+    end
+  end
+
+  def update
+    @post = current_user.votes.find(params[:id]).post
+    if status == 'trash'
+      current_user.update_trash(@post)
+      redirect_back fallback_location: root_path
+    elsif status == 'untrash'
+      current_user.update_untrash(@post)
+      redirect_back fallback_location: root_path
+    end
   end
 
   def destroy
@@ -13,6 +30,10 @@ class VotesController < ApplicationController
   private
 
   def vote_params
-    params.permit(:status).merge(post_id: params[:post_id])
+    params.require(:vote).permit(:status).merge(post_id: params[:post_id])
+  end
+
+  def vote_update_params
+    params.require(:vote).permit(:status).merge(post_id: params[:post_id])
   end
 end
