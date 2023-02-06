560344613009c2eddff99bd7c8035de39b15a8cf
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-02-06 19:22:34 +0900

    Add:Tos&PP

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'login', to: 'user_sessions#new'
   post 'login', to: 'user_sessions#create'
   get 'logout', to: 'user_sessions#destroy'
+  get 'tos', to: 'pages#tos'
+  get 'pp', to: 'pages#pp'
   resources :users, only: %i[new create]
   resources :posts do
     resources :comments, only: %i[create update destroy], shallow: true do
