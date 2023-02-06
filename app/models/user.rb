e43d891afc38fdd7c38531dcb47e7808795da50c
Yuto307 <harubaruharu_over3010@yahoo.co.jp>
2023-02-06 19:35:34 +0900

    Fix:Tos&PP

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,6 +12,7 @@ class User < ApplicationRecord
   validates :password, length: { minimum: 3 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+  validates_acceptance_of :agreement, allow_nil: false, on: :create
 
   validates :name, presence: true, length: { maximum: 255 }
 
